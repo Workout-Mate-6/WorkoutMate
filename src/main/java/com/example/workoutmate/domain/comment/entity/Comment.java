4c30f5c50607622038acf2f743ac71dd29c93513
aneunwook <dmsdnr0720@naver.com>
2025-07-21 17:04:43 +0900

    feat(comment) : comment 엔티티 작성

--- src/main/java/com/example/workoutmate/domain/comment/entity/Comment.java
+++ src/main/java/com/example/workoutmate/domain/comment/entity/Comment.java
@@ -1,6 +1,48 @@
 package com.example.workoutmate.domain.comment.entity;
 
+import com.example.workoutmate.domain.board.entity.Board;
+import com.example.workoutmate.domain.user.entity.User;
 import com.example.workoutmate.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE comment SET is_deleted = true, deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@Where(clause = "is_deleted = false")
+@Builder
 public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id", nullable = false)
+    private User writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    // 댓글 수정
+    public void updateComment(String content){
+        this.content = content;
+    }
 }
