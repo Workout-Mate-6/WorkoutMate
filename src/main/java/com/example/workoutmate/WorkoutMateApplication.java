2fe2a24843fd81536d4fd36636b81213c452b022
doohaaa <engk5423@naver.com>
2025-07-21 11:31:32 +0900

    chore: 프로젝트 초기설정

---
+++ src/main/java/com/example/workoutmate/WorkoutMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.workoutmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutMateApplication.class, args);
+    }
+
+}
