804fcef206c5d252ba4f4d7fcf172a3422c2cbaf
LeeHyeonha <lej5769@gmail.com>
2025-07-21 19:48:58 +0900

    feat: UserRepository 어노테이션 추가

--- src/main/java/com/example/workoutmate/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/workoutmate/domain/user/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.example.workoutmate.domain.user.repository;
 
 import com.example.workoutmate.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmailAndIsDeletedFalse(String email);
     Optional<User> findByEmailAndIsDeletedFalse(String email);
