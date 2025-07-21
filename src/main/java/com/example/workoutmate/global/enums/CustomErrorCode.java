05990698883379fd8700e285b8bdbc595a862420
LeeHyeonha <lej5769@gmail.com>
2025-07-21 15:33:21 +0900

    feat: USER NOT FOUND 에러 enums 추가

--- src/main/java/com/example/workoutmate/global/enums/CustomErrorCode.java
+++ src/main/java/com/example/workoutmate/global/enums/CustomErrorCode.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CustomErrorCode {
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 오류"),
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.");
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "유저를 찾을 수 없습니다.");
 
 
     private final HttpStatus httpStatus;
