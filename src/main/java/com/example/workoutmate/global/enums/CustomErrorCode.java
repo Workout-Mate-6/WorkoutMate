25da32ed3e23ec860424244ded9a8c1abe6deab6
doohaaa <engk5423@naver.com>
2025-07-21 14:43:12 +0900

    feat(security): Spring Security, Jwt 기반 인증, 인가 기능 구현

--- src/main/java/com/example/workoutmate/global/enums/CustomErrorCode.java
+++ src/main/java/com/example/workoutmate/global/enums/CustomErrorCode.java
@@ -6,8 +6,14 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CustomErrorCode {
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 오류"),
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.");
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 
+    // 인증 인가
+    SERVER_EXCEPTION_JWT(HttpStatus.INTERNAL_SERVER_ERROR, "Not Found Token"),
+    SC_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 서명입니다."),
+    SC_BAD_REQUEST(HttpStatus.BAD_REQUEST,"지원되지 않는 JWT 토큰입니다."),
+    SC_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러")
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
