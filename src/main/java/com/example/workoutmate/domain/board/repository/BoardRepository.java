086bead837ea2de20afd04d9b7eb2bd2347f70da
hyunmin <khmin052@naver.com>
2025-07-21 16:00:47 +0900

    feat(Board): BoardRepository JpaRepository 상속 BoardRepository 인터페이스 파일을 JpaRepository 상속으로 수정

--- src/main/java/com/example/workoutmate/domain/board/repository/BoardRepository.java
+++ src/main/java/com/example/workoutmate/domain/board/repository/BoardRepository.java
@@ -1,4 +1,8 @@
 package com.example.workoutmate.domain.board.repository;
 
-public interface BoardRepository {
+import com.example.workoutmate.domain.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
 }
