e29670d2e3422677554d44bab7f88c41b8541c22
LeeHyeonha <lej5769@gmail.com>
2025-07-21 16:27:02 +0900

    feat: email로 유저 찾는 로직 추가

--- src/main/java/com/example/workoutmate/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/workoutmate/domain/user/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.workoutmate.domain.user.repository;
 
-public interface UserRepository {
+import com.example.workoutmate.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
