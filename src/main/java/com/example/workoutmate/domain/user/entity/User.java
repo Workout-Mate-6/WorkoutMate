1baf59c88995c50ade43b26ec638ba818435629e
LeeHyeonha <lej5769@gmail.com>
2025-07-21 14:19:05 +0900

    feat: user entity 생성

--- src/main/java/com/example/workoutmate/domain/user/entity/User.java
+++ src/main/java/com/example/workoutmate/domain/user/entity/User.java
@@ -1,6 +1,47 @@
 package com.example.workoutmate.domain.user.entity;
 
 import com.example.workoutmate.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
 public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+    private LocalDateTime deletedAt;
+
+    public User(String email, String password, String name, String gender, String role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.gender = gender;
+        this.role = role;
+    }
 }
