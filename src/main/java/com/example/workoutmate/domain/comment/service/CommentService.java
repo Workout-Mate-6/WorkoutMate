12ad58ecaf3ad9137d12c3d8359af228c3206850
aneunwook <dmsdnr0720@naver.com>
2025-07-22 14:05:14 +0900

    feat(Comment) : 댓글 수정 API 구현

--- src/main/java/com/example/workoutmate/domain/comment/service/CommentService.java
+++ src/main/java/com/example/workoutmate/domain/comment/service/CommentService.java
@@ -10,11 +10,16 @@ import com.example.workoutmate.domain.comment.repository.CommentRepository;
 import com.example.workoutmate.domain.user.entity.User;
 import com.example.workoutmate.domain.user.service.UserService;
 import com.example.workoutmate.global.config.CustomUserPrincipal;
+import com.example.workoutmate.global.enums.CustomErrorCode;
+import com.example.workoutmate.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static com.example.workoutmate.global.enums.CustomErrorCode.COMMENT_NOT_IN_BOARD;
+import static com.example.workoutmate.global.enums.CustomErrorCode.UNAUTHORIZED_COMMENT_ACCESS;
 
 
 @Service
@@ -46,5 +51,32 @@ public class CommentService {
                 .stream().map(CommentMapper::data).toList();
 
         return comments;
+    }
+
+    @Transactional
+    public CommentResponseDto updateComment(Long boardId, Long commentId, CommentRequestDto requestDto, CustomUserPrincipal authUser) {
+        Board board = boardService.getBoardById(boardId);
+        Comment comment = findById(commentId);
+
+        if (!comment.getBoard().getId().equals(board.getId())){
+            throw new CustomException(COMMENT_NOT_IN_BOARD, COMMENT_NOT_IN_BOARD.getMessage());
+        }
+
+        User user = userService.findById(authUser.getId());
+
+        if (!comment.getWriter().getId().equals(user.getId())) {
+            throw new CustomException(UNAUTHORIZED_COMMENT_ACCESS);
+        }
+
+        comment.updateComment(requestDto.getContent());
+
+        commentRepository.flush();
+
+        return CommentMapper.data(comment);
+    }
+
+    public Comment findById(Long commentId){
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new CustomException(CustomErrorCode.COMMENT_NOT_FOUND));
     }
 }
