ab064c0db6a2de108759c8b4bce02e09c80e0632
LeeHyeonha <lej5769@gmail.com>
2025-07-22 11:21:07 +0900

    comment(user): UserRepositoryCustom 임시 주석처리

--- src/main/java/com/example/workoutmate/domain/user/repository/UserRepositoryCustomImpl.java
+++ src/main/java/com/example/workoutmate/domain/user/repository/UserRepositoryCustomImpl.java
@@ -1,9 +1,9 @@
 package com.example.workoutmate.domain.user.repository;
 
 import com.example.workoutmate.domain.user.dto.UserInfoResponseDto;
-import com.example.workoutmate.domain.user.entity.QUser;
+//import com.example.workoutmate.domain.user.entity.QUser;
 import com.example.workoutmate.domain.user.entity.User;
-import com.querydsl.jpa.impl.JPAQueryFactory;
+//import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -11,8 +11,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserRepositoryCustomImpl implements UserRepositoryCustom {
 
-    private final JPAQueryFactory queryFactory;
-    private static final QUser user = QUser.user;
+//    private final JPAQueryFactory queryFactory;
+//    private static final QUser user = QUser.user;
 
     @Override
     public UserInfoResponseDto getUserInfo() {
