dce7d425821bad7f0a1371f7ef860335a37b4ca7
taehyeon <max5915@naver.com>
2025-07-22 11:20:11 +0900

    feat(follower) : 팔로워 목록 조회 구현 완료 <쿼리DSL 추가했습니다.>

--- src/main/java/com/example/workoutmate/domain/follow/controller/FollowController.java
+++ src/main/java/com/example/workoutmate/domain/follow/controller/FollowController.java
@@ -1,5 +1,6 @@
 package com.example.workoutmate.domain.follow.controller;
 
+import com.example.workoutmate.domain.follow.dto.FollowsResponseDto;
 import com.example.workoutmate.domain.follow.service.FollowService;
 import com.example.workoutmate.global.config.CustomUserPrincipal;
 import com.example.workoutmate.global.dto.ApiResponse;
@@ -7,10 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,8 +23,17 @@ public class FollowController {
     public ResponseEntity<ApiResponse<Void>> followUser(
             @PathVariable Long userId,
             @AuthenticationPrincipal CustomUserPrincipal authUser
-            ) {
+    ) {
         followService.follow(userId, authUser);
         return ApiResponse.success(HttpStatus.OK, "팔로우 하였습니다.", null);
+    }
+
+    @GetMapping("/follows/follower/{userId}")
+    public ResponseEntity<ApiResponse<List<FollowsResponseDto>>> follower(
+            @PathVariable Long userId,
+            @RequestParam(defaultValue = "10") Integer size,
+            @RequestParam(required = false) Long cursor
+    ) {
+        return ApiResponse.success(HttpStatus.OK, "", followService.viewFollower(userId, size, cursor));
     }
 }
