05423f10a5ace26b664f30fabf28dd6113285a16
doohaaa <engk5423@naver.com>
2025-07-24 12:45:40 +0900

    feat(board+): 엔티티에 필드 추가

--- src/main/java/com/example/workoutmate/domain/board/entity/Board.java
+++ src/main/java/com/example/workoutmate/domain/board/entity/Board.java
@@ -1,5 +1,6 @@
 package com.example.workoutmate.domain.board.entity;
 
+import com.example.workoutmate.domain.board.enums.Status;
 import com.example.workoutmate.domain.user.entity.User;
 import com.example.workoutmate.global.entity.BaseEntity;
 import jakarta.persistence.*;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Entity
 @Getter
 @NoArgsConstructor
@@ -48,20 +50,38 @@ public class Board extends BaseEntity {
     @Column
     private LocalDateTime deletedAt;
 
+    // 모집 인원
+    @Column(name = "target_count", nullable = false)
+    private Long targetCount;
+
+    // 모집된 인원
+    @Column(name = "current_count")
+    @Builder.Default
+    private Long currentCount = 0L;
+
+    // 모집 상태
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status = Status.OPEN;
+
+
     @Builder
-    public Board(User writer, String title, String content, SportType sportType) {
+    public Board(User writer, String title, String content, SportType sportType, Long targetCount) {
         this.writer = writer;
         this.title = title;
         this.content = content;
         this.sportType = sportType;
+        this.targetCount = targetCount;
         this.isDeleted = false;
         this.deletedAt = null;
     }
 
-    public void update(String title, String content, SportType sportType) {
+    public void update(String title, String content, SportType sportType, Long targetCount) {
         this.title = title;
         this.content = content;
         this.sportType = sportType;
+        this.targetCount = targetCount;
     }
 
     public void delete() {
