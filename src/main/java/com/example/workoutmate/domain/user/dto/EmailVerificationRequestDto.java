7ed33e14df02709830c767b7cd910094b83bd19b
aneunwook <dmsdnr0720@naver.com>
2025-07-25 12:31:57 +0900

    fix(auth) : @Validation 어노테이션 추가

--- src/main/java/com/example/workoutmate/domain/user/dto/EmailVerificationRequestDto.java
+++ src/main/java/com/example/workoutmate/domain/user/dto/EmailVerificationRequestDto.java
@@ -1,9 +1,16 @@
 package com.example.workoutmate.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class EmailVerificationRequestDto {
+
+    @NotBlank
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
+
+    @NotBlank
     private String code;
 }
