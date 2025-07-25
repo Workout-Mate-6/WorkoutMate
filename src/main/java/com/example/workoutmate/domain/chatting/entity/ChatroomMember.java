a0d587d4a43c2191fbf9fa2832d29c2417a1a74e
LeeHyeonha <lej5769@gmail.com>
2025-07-25 16:03:20 +0900

    feat(chatting): ChatroomMember entity 추가

---
+++ src/main/java/com/example/workoutmate/domain/chatting/entity/ChatroomMember.java
@@ -0,0 +1,38 @@
+package com.example.workoutmate.domain.chatting.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "chatroom_member")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatroomMember {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Long chatroomId;
+
+    @Column(name = "joined_at")
+    private LocalDateTime joinedAt;
+
+    @Column(name = "left_at")
+    private LocalDateTime leftAt;
+
+    @Column(name = "is_joined")
+    @Builder.Default
+    private boolean isJoined = true;
+}
