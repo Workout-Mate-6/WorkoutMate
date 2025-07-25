5bd754034b25bf649488aa4db3fb9496e90d0cc4
LeeHyeonha <lej5769@gmail.com>
2025-07-25 16:02:38 +0900

    feat(chatting): Chatroom entity 추가

---
+++ src/main/java/com/example/workoutmate/domain/chatting/entity/Chatroom.java
@@ -0,0 +1,47 @@
+package com.example.workoutmate.domain.chatting.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "chatroom")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Chatroom {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long senderId;
+
+    @NotNull
+    private Long receiverId;
+
+    private String lastMessage;
+
+    private LocalDateTime lastChatTime;
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "is_deleted")
+    @Builder.Default
+    private boolean isDeleted = false;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+
+}
