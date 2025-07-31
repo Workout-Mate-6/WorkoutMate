8cd543f21cba17e4b2adfeefe6db946f488883bc
aneunwook <dmsdnr0720@naver.com>
2025-07-31 09:56:19 +0900

    refactor(auth) : 회원가입 시 이름 20자로 제약

--- src/main/java/com/example/workoutmate/domain/user/dto/SignupRequestDto.java
+++ src/main/java/com/example/workoutmate/domain/user/dto/SignupRequestDto.java
@@ -5,6 +5,7 @@ import com.example.workoutmate.domain.user.enums.UserGender;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,6 +24,7 @@ public class SignupRequestDto {
     private String password;
 
     @NotBlank
+    @Size(max = 20, message = "이름은 최대 20자까지 입력이 가능합니다.")
     private String name;
 
     @NotNull
