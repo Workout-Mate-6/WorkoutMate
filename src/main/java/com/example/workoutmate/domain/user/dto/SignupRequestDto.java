4fcb130ef3241fc5850f7a5b9e686c126294f2ab
taehyeon <max5915@naver.com>
2025-07-31 10:12:11 +0900

    feat(exception-handling) : 회원가입시 성별 빈값이거나 정해둔 성별 외의 것이 들어오면 오류메세지 제대로 나오도록 구현

--- src/main/java/com/example/workoutmate/domain/user/dto/SignupRequestDto.java
+++ src/main/java/com/example/workoutmate/domain/user/dto/SignupRequestDto.java
@@ -5,6 +5,7 @@ import com.example.workoutmate.domain.user.enums.UserGender;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,6 +26,7 @@ public class SignupRequestDto {
     @NotBlank
     private String name;
 
-    @NotNull
-    private UserGender gender;
+    @NotNull(message = "성별이 Null 일 수 는 없습니다!")
+    @Pattern(regexp = "Male|Female", message = "올바른 성별을 입력해주세요.")
+    private String gender;
 }
