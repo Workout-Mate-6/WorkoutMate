bdb1e14bca736d7f5cee643376f52ee1aae413a8
aneunwook <dmsdnr0720@naver.com>
2025-08-01 11:39:51 +0900

    feat(board-popular) : 레디스 의존성 추가 및 환경변수에 포트 설정

--- src/main/java/com/example/workoutmate/WorkoutMateApplication.java
+++ src/main/java/com/example/workoutmate/WorkoutMateApplication.java
@@ -2,9 +2,10 @@ package com.example.workoutmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class WorkoutMateApplication {
 
     public static void main(String[] args) {
