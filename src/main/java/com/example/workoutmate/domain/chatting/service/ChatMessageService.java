06f10c4e2861eb6cf12bb6ff6cc52864c3eadf26
LeeHyeonha <lej5769@gmail.com>
2025-08-05 20:29:30 +0900

    feat(chatting): 입장 메시지, 퇴장 메시지 구현 및 메시지 발행 책임 분리

--- src/main/java/com/example/workoutmate/domain/chatting/service/ChatMessageService.java
+++ src/main/java/com/example/workoutmate/domain/chatting/service/ChatMessageService.java
@@ -3,6 +3,7 @@ package com.example.workoutmate.domain.chatting.service;
 import com.example.workoutmate.domain.chatting.dto.ChatDto;
 import com.example.workoutmate.domain.chatting.entity.ChatMessage;
 import com.example.workoutmate.domain.chatting.entity.ChattingMapper;
+import com.example.workoutmate.domain.chatting.event.ChatPublisher;
 import com.example.workoutmate.domain.chatting.repository.ChatMessageRepository;
 import com.example.workoutmate.domain.user.entity.User;
 import com.example.workoutmate.domain.user.service.UserService;
@@ -16,9 +17,10 @@ public class ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
     private final UserService userService;
+    private final ChatPublisher chatPublisher;
 
     @Transactional
-    public ChatDto save(ChatDto chatDto, String email) {
+    public void save(ChatDto chatDto, String email) {
         User sender = userService.findByEmail(email);
 
         chatDto.setTypeTalk();
@@ -26,6 +28,8 @@ public class ChatMessageService {
 
         chatMessageRepository.save(chatMessage);
 
-        return ChattingMapper.toChatDto(chatMessage);
+        ChatDto chat = ChattingMapper.toChatDto(chatMessage);
+
+        chatPublisher.sendMessage(chat.getChatRoomId(), chat);
     }
 }
