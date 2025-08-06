d8b2fd663dd34fa8c06739ca3c5e1aad2161be1e
taehyeon <max5915@naver.com>
2025-08-06 10:49:10 +0900

    fix(personalization) : 이제 많이 참여하는 운동타입에 높은 점수 반영

--- src/main/java/com/example/workoutmate/domain/recommend/calculator/TypeScoreCalculator.java
+++ src/main/java/com/example/workoutmate/domain/recommend/calculator/TypeScoreCalculator.java
@@ -2,6 +2,7 @@ package com.example.workoutmate.domain.recommend.calculator;
 
 import com.example.workoutmate.domain.board.entity.Board;
 import com.example.workoutmate.domain.board.entity.SportType;
+import com.example.workoutmate.domain.participation.entity.Participation;
 import com.example.workoutmate.domain.participation.service.ParticipationService;
 import com.example.workoutmate.domain.recommend.dto.UserActivityData;
 import com.example.workoutmate.domain.user.entity.User;
@@ -14,7 +15,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
-public class TypeScoreCalculator implements ScoreCalculator{
+public class TypeScoreCalculator implements ScoreCalculator {
     @Override
     public String getScoreType() {
         return "sportType";
@@ -25,10 +26,25 @@ public class TypeScoreCalculator implements ScoreCalculator{
                                               UserActivityData activityData, ParticipationService participationService,
                                               ZzimService zzimService
     ) {
-        Set<SportType> preferredTypes = activityData.getPreferredSportTypes();
+        List<Participation> participations = activityData.getParticipations();
+        if (participations == null || participations.isEmpty()) {
+            // 참여 기록 없으면 전부 0점
+            return boards.stream().collect(Collectors.toMap(Board::getId, b -> 0.0));
+        }
 
-        return boards.stream().collect(Collectors.toMap(Board::getId,
-                board -> preferredTypes.contains(board.getSportType()) ? 1.0 : 0.0
+        // 운동 타입마다 참여횟수 확인
+        Map<SportType, Long> typeCount = participations.stream()
+                .collect(Collectors.groupingBy(p -> p.getBoard().getSportType(), Collectors.counting()));
+
+        // 전체 참여 횟수
+        long total = typeCount.values().stream().mapToLong(Long::longValue).sum();
+        if (total == 0) {
+            return boards.stream().collect(Collectors.toMap(Board::getId, b -> 0.0));
+        }
+        return boards.stream().collect(Collectors.toMap(Board::getId, board -> {
+                    long count = typeCount.getOrDefault(board.getSportType(), 0L);
+                    return (double) count / total;
+                }
         ));
     }
 }
