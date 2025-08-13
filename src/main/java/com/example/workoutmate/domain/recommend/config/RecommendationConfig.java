9e4d04a5c26d76fec9ef3f59f8cda2c0ecb748ee
taehyeon <max5915@naver.com>
2025-08-13 12:52:45 +0900

    fix(recommend) : 개인화 새로 구현하면서 충돌 일어나는거 주석 처리

--- src/main/java/com/example/workoutmate/domain/recommend/config/RecommendationConfig.java
+++ src/main/java/com/example/workoutmate/domain/recommend/config/RecommendationConfig.java
@@ -8,23 +8,23 @@ import org.springframework.stereotype.Component;
 
 
 
-@Component
-@Data
-@ConfigurationProperties(prefix = "recommendation")
-public class RecommendationConfig {
-    private Weights weights; // 가중치를 의미함
-    private int maxCandidateSize; // 후보군 갯수를 의미
-    private double minScore;
-
-    @Getter
-    @Setter
-    public static class Weights {
-        private double participation;
-        private double zzim;
-        private double friend;
-        private double type;
-        private double time;
-
-    }
-
-}
+//@Component
+//@Data
+//@ConfigurationProperties(prefix = "recommendation")
+//public class RecommendationConfig {
+//    private Weights weights; // 가중치를 의미함
+//    private int maxCandidateSize; // 후보군 갯수를 의미
+//    private double minScore;
+//
+//    @Getter
+//    @Setter
+//    public static class Weights {
+//        private double participation;
+//        private double zzim;
+//        private double friend;
+//        private double type;
+//        private double time;
+//
+//    }
+//
+//}
